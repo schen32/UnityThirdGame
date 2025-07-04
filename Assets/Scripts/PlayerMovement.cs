50ce93840742d9c9f34e511786eb6c9a4860d468
Simon Chen <simonc8192@gmail.com>
2025-07-03 21:27:48 -0700

    Add project files.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+
+    SpriteRenderer m_spriteRenderer;
+    Vector2 m_moveAmount = Vector2.zero;
+
+    void Awake()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (m_moveAmount.sqrMagnitude > 0)
+        {
+            transform.position += new Vector3(m_moveAmount.x, m_moveAmount.y, 0) * moveSpeed * Time.deltaTime;
+            m_spriteRenderer.color = Color.red;
+        }
+        else
+        {
+            m_spriteRenderer.color = Color.white;
+        }
+        
+    }
+    public void OnMove(InputValue value)
+    {
+        m_moveAmount = value.Get<Vector2>();
+    }
+}
