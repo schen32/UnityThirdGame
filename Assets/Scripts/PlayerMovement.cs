9f32adadbc508d0d91b17bd9c7ec29e170691a31
Simon Chen <simonc8192@gmail.com>
2025-07-05 06:42:03 -0700

    - player idle and run animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,15 @@ public class PlayerMovement : MonoBehaviour
     public float m_moveSpeed = 5.0f;
 
     SpriteRenderer m_spriteRenderer;
+    Animator m_animator;
+
     Vector2 m_moveAmount = Vector2.zero;
+    bool m_isMoving = false;
 
     void Awake()
     {
         m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -18,8 +22,9 @@ public class PlayerMovement : MonoBehaviour
         if (m_moveAmount.sqrMagnitude > 0)
         {
             transform.position += new Vector3(m_moveAmount.x, m_moveAmount.y, 0) * m_moveSpeed * Time.deltaTime;
+            m_isMoving = true;
             
-            if (m_moveAmount.x >= 0)
+            if (m_moveAmount.x > 0)
             {
                 m_spriteRenderer.flipX = false;
             }
@@ -28,6 +33,11 @@ public class PlayerMovement : MonoBehaviour
                 m_spriteRenderer.flipX = true;
             }
         }
+        else
+        {
+            m_isMoving = false;
+        }
+        m_animator.SetBool("isMoving", m_isMoving);
         
     }
     public void OnMove(InputValue value)
