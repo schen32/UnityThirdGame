f67d01758418b576679664f2e46a0253e83bf579
Simon Chen <simonc8192@gmail.com>
2025-07-11 07:02:32 -0700

    - imported UI images

--- Assets/Scripts/EnemyAttackEffect.cs
+++ Assets/Scripts/EnemyAttackEffect.cs
@@ -4,7 +4,6 @@ public class EnemyAttackEffect : MonoBehaviour
     public int m_attackDamage = 1;
     public float m_knockbackForce = 5f;
     public float m_knockbackDuration = 0.5f;
-    public float m_knockbackDamping = 5f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy")) return;
